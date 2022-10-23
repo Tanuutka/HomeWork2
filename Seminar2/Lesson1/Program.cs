ce8985b8cb18d396d25736f4a50df8de84fbcc61
Tanuutka <Tanuutka@yandex.ru>
2022-10-23 21:20:40 +0300

    Решение задач

---
+++ Seminar2/Lesson1/Program.cs
@@ -0,0 +1,13 @@
+﻿// Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+Console.Write("Введите число: ");
+int number = Convert.ToInt32 (Console.ReadLine());
+
+if ((number >99 && number <= 999) || (number >= -999 && number <-99))
+{
+    Console.Write("Вторая цифра равна: "+ (Math.Abs(number)/10)%10);
+}
+else 
+{
+    Console.Write("Введенное число не трехзначное");
+}
+
---
+++ Seminar2/Lesson2/Program.cs
@@ -0,0 +1,18 @@
+﻿// Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+Console.Write("Введите число: ");
+int number = Convert.ToInt32 (Console.ReadLine());
+
+if (number < 100 && number > - 100)
+{
+  Console.Write("третьей цифры нет: ");
+}
+else
+{
+   while (number > 999 || number < -999) 
+   {
+    number = number/10;
+   }
+ 
+ Console.Write("третья цифра равна: "+ Math.Abs(number)%10);
+}
+
